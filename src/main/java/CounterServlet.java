f18da1fffb400fb349ba2285d88f60f8a9751f8b
Milianigia Rios <nanirios671@gmail.com>
2023-10-31 14:19:26 -0500

    finished with the exercise

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name= "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+    protected int count = 0;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+        count++;
+        //this is what displays the text on the webpage
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Number of times visited:  " + count + "</h1>");
+    }
+}
