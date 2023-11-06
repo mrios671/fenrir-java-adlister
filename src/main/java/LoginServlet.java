d8f29327b5e2659c0729318ec0195ed80b0fd22d
Milianigia Rios <nanirios671@gmail.com>
2023-11-06 12:08:07 -0600

    committing now, going to eat lunch.

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (username.equals("admin") && password.equals("password")) {
+            response.sendRedirect("/profile");
+        }
+    }
+}
---
+++ src/main/webapp/ads/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: milianigiarios
+  Date: 11/6/23
+  Time: 10:57 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Index</title>
+</head>
+<body>
+
+</body>
+</html>
